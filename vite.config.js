40eaaea719bc9e09e15bb2c3112bfb349056f971
Shafahat565 <shafahatnisar@gmail.com>
2025-08-25 23:38:12 +0500

    feat: describe your changes

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   
   plugins: [
+    
     tailwindcss(),
     react()],
-    base: '/Templato/',
     
+  
+
    
    
 })
