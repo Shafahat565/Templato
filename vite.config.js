df203ef249cd443fde926aee6917db84dfbf278a
Shafahat565 <shafahatnisar@gmail.com>
2025-08-26 00:33:01 +0500

    hello2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const isGH = process.env.NODE_ENV === 'production'
 // https://vite.dev/config/
 export default defineConfig({
   
@@ -9,7 +11,7 @@ export default defineConfig({
     
     tailwindcss(),
     react()],
-     base: '/Templato/',
+     base: isGH ? '/Templato' : '/',
   
 
    
